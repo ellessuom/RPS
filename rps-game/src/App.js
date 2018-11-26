b71ad68ae6f79dc947bb9da586e9412efcaab43d
Demousselle, Agustin <Agustin.Demousselle@ncr.com>
2018-11-26 16:49:01 +0000

    Removes unused stuff from boilerplate and adds initial setup

--- rps-game/src/App.js
+++ rps-game/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Stats from './components/Stats';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      won: 0,
+      lost: 0
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Stats won={this.state.won} lost={this.state.lost}/>
       </div>
     );
   }
---
+++ rps-game/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render () {
+        return (
+            <header>
+                <h1>RPS GAME</h1>
+            </header>
+        )
+    }
+}
